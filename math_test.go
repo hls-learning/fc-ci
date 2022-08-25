722de634894a957ad47c0e8f014f71472e16fc18
Henrique Luiz <henriqueluiz_silva@yahoo.com.br>
2022-08-25 20:51:11 -0300

    ci: ajustado o codigo para passar no teste

--- math_test.go
+++ math_test.go
@@ -9,3 +9,27 @@ func TestSoma(t *testing.T) {
 		t.Errorf("Resutado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
+
+func TestSubtrai(t *testing.T) {
+	total := Subtrai(15, 15)
+
+	if total != 0 {
+		t.Errorf("Resutado da soma é inválido: Resultado %d. Esperado: %d", total, 0)
+	}
+}
+
+func TestMultiplica(t *testing.T) {
+	total := Multiplica(15, 15)
+
+	if total != 225 {
+		t.Errorf("Resutado da soma é inválido: Resultado %d. Esperado: %d", total, 225)
+	}
+}
+
+func TestDivide(t *testing.T) {
+	total := Divide(15, 15)
+
+	if total != 1 {
+		t.Errorf("Resutado da soma é inválido: Resultado %d. Esperado: %d", total, 1)
+	}
+}
